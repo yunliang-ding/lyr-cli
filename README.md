5d4ae3104aab427fd84fa18f99e5e0bf957a8ec3
yunliang-ding <804408086@qq.com>
2021-03-20 14:28:19 +0800

    version

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ dyl-cli
 npm install dyl-cli -g
 ```
 
-
 初始化命令
 ```
 dyl-cli init <projectName>
@@ -19,27 +18,4 @@ dyl-cli  i <projectName>
 版本号查看
 ```
 dyl-cli -v
-```
-# git仓库
-```
-4.添加远程仓库
-git remote add origin https://XXX.git
-
-5.本地仓库也远程仓库关联
-git fetch // 拉取远程分之
-git checkout -b master
-git checkout master // 切换到master分支
-git branch --set-upstream-to=origin/master master
-
-6.拉取远程仓库内容到本地
-这时候用git pull会提示(毕竟本地和远程仓库没啥关系指针连接不起来的缘故吧)：
-
-fatal: refusing to merge unrelated histories
-
-因此命令应该改为：
-
-git pull origin master --allow-unrelated-histories  
-
-7.将最新的内容推送到远程仓库
-git push
 ```
--- bin/config/index.js
+++ bin/config/index.js
@@ -2,25 +2,30 @@
   @dest: 使用配置文件
  */
 module.exports = {
-  npmUrl: 'https://registry.npmjs.org/shc-cli/latest',
-  projectList: [{
-    type: 'list',
-    message: '请选择拉取的模版类型:',
-    name: 'type',
-    choices: [{
-      name: 'mobile',
-      value: {
-        url: 'https://github.com/yunliang-ding/react-mobile-template.git',
-        gitName: 'react-mobile-template',
-        val: '移动端模版'
-      }
-    }, {
-      name: 'pc',
-      value: {
-        url: 'https://github.com/yunliang-ding/react-pc-template.git',
-        gitName: 'react-pc-template',
-        val: 'PC端模版'
-      }
-    }]
-  }],
+  npmUrl: "https://registry.npmjs.org/shc-cli/latest",
+  projectList: [
+    {
+      type: "list",
+      message: "请选择拉取的模版类型:",
+      name: "type",
+      choices: [
+        {
+          name: "pc",
+          value: {
+            url: "https://github.com/yunliang-ding/react-mobx-template.git",
+            gitName: "react-mobx-template",
+            val: "react-base",
+          },
+        },
+        {
+          name: "pc",
+          value: {
+            url: "https://github.com/yunliang-ding/react-base-template.git",
+            gitName: "react-base-template",
+            val: "react-mobx",
+          },
+        },
+      ],
+    },
+  ],
 };
